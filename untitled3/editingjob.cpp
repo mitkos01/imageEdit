f8f42971b57c43d37656760f671ebea678cf18b7
mitkos01 <141376492+mitkos01@users.noreply.github.com>
2024-04-17 16:49:59 +0300

    Add files via upload

--- untitled3/editingjob.cpp
+++ untitled3/editingjob.cpp
@@ -6,26 +6,36 @@ EditingJob::EditingJob(int totalImages)
 
 void EditingJob::distributeWork(std::vector<Editor>& editors) {
 
-    double totalRate = 0.0; // Розрахунок загальної швидкості редакторів
-    for (const auto& editor : editors) {
-        totalRate += editor.getRate();
+    double totalRate = std::accumulate(editors.begin(), editors.end(), 0.0,
+                                       [](double sum, const Editor& editor) {
+                                           return sum + editor.getRate();
+                                       });
+
+
+    int remainingImages = totalImages_;
+    for (auto& editor : editors) {
+        int imagesForThisEditor = static_cast<int>((editor.getRate() / totalRate) * totalImages_);
+        editor.editImages(imagesForThisEditor);
+        remainingImages -= imagesForThisEditor;
     }
 
 
-    for (auto& editor : editors) { // Розподіл роботи між редакторами
-        int imagesForThisEditor = static_cast<int>((editor.getRate() / totalRate) * totalImages_);
-        editor.editImages(imagesForThisEditor);
+    int editorIndex = 0;
+    while (remainingImages > 0) {
+        editors[editorIndex].editImages(1);
+        --remainingImages;
+        editorIndex = (editorIndex + 1) % editors.size(); 
     }
 }
 
 int EditingJob::calculateTotalTime(const std::vector<Editor>& editors) const {
+    
+    double totalRate = std::accumulate(editors.begin(), editors.end(), 0.0,
+                                       [](double sum, const Editor& editor) {
+                                           return sum + editor.getRate();
+                                       });
 
-    double totalRate = 0.0; // Розрахунок загальної швидкості редакторів
-    for (const auto& editor : editors) {
-        totalRate += editor.getRate();
-    }
-
-
-    double totalTime = static_cast<double>(totalImages_) / totalRate; // Розрахунок загального часу
+    
+    double totalTime = static_cast<double>(totalImages_) / totalRate;
     return static_cast<int>(totalTime);
 }
